2e60d204b4aa90df05f9f1c877926a66e85322d4
LGUG2Z <LGUG2Z@fastmail.com>
2023-10-01 05:11:19 +0000

    feat(nix): add optional remote vscode config

--- wsl.nix
+++ wsl.nix
@@ -67,6 +67,22 @@
     autoPrune.enable = true;
   };
 
+  # FIXME: uncomment the next block to make vscode running in Windows "just work" with NixOS on WSL
+  # solution adapted from: https://github.com/K900/vscode-remote-workaround
+  # more information: https://github.com/nix-community/NixOS-WSL/issues/238 and https://github.com/nix-community/NixOS-WSL/issues/294
+  # systemd.user = {
+  #   paths.vscode-remote-workaround = {
+  #     wantedBy = ["default.target"];
+  #     pathConfig.PathChanged = "%h/.vscode-server/bin";
+  #   };
+  #   services.vscode-remote-workaround.script = ''
+  #     for i in ~/.vscode-server/bin/*; do
+  #       echo "Fixing vscode-server in $i..."
+  #       ln -sf ${pkgs.nodejs_18}/bin/node $i/node
+  #     done
+  #   '';
+  # };
+
   nix = {
     settings = {
       trusted-users = [username];
