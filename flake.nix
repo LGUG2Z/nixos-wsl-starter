23660c6593ad0c2880182694140469ec8fdc2d30
LGUG2Z <LGUG2Z@fastmail.com>
2023-12-01 01:07:20 +0000

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/28535c3a34d79071f2ccb68671971ce0c0984d7e' (2023-11-19)
      → 'github:nix-community/home-manager/aeb2232d7a32530d3448318790534d196bf9427a' (2023-11-24)
    • Updated input 'nixos-wsl':
        'github:nix-community/NixOS-WSL/a9287f7191467138d6203ea44b3a0b9c745cb145' (2023-11-22)
      → 'github:nix-community/NixOS-WSL/1a50ab3fe98934d8f109f05528874c9ab324707b' (2023-11-30)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/d2e4de209881b38392933fabf303cde3454b0b4c' (2023-11-26)
      → 'github:nixos/nixpkgs/50aa30a13c4ab5e7ba282da460a3e3d44e9d0eb3' (2023-11-29)
    • Updated input 'nixpkgs-unstable':
        'github:nixos/nixpkgs/5a09cb4b393d58f9ed0d9ca1555016a8543c2ac8' (2023-11-24)
      → 'github:nixos/nixpkgs/8cfef6986adfb599ba379ae53c9f5631ecd2fd9c' (2023-11-27)
    • Updated input 'nur':
        'github:nix-community/NUR/82fc3d559e4e1cf99618ffd7f5f5f1ea26509763' (2023-11-27)
      → 'github:nix-community/NUR/38a400bdf1c847cb7b7ff0fc9c13492e6dfd2708' (2023-11-30)

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   description = "NixOS configuration";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
   inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  inputs.home-manager.url = "github:nix-community/home-manager/release-23.05";
+  inputs.home-manager.url = "github:nix-community/home-manager/release-23.11";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
   inputs.nur.url = "github:nix-community/NUR";
