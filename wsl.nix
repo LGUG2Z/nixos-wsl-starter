4d5f8a85bdd870fabe99286e5994eb30527456d7
tahgijones@gmail.com <Monaciello>
2024-07-11 12:41:05 -0700

    edits approved sops-nix

--- wsl.nix
+++ wsl.nix
@@ -1,6 +1,8 @@
 {
   # FIXME: uncomment the next line if you want to reference your GitHub/GitLab access tokens and other secrets
   # secrets,
+  lib,
+  config,
   username,
   hostname,
   pkgs,
@@ -8,38 +10,34 @@
   ...
 }: {
   # FIXME: change to your tz! look it up with "timedatectl list-timezones"
-  time.timeZone = "America/Los_Angeles";
+  time.timeZone = "America/New_York";
 
   networking.hostName = "${hostname}";
 
-  # FIXME: change your shell here if you don't want fish
   programs.fish.enable = true;
   environment.pathsToLink = ["/share/fish"];
   environment.shells = [pkgs.fish];
 
   environment.enableAllTerminfo = true;
 
+  sops.secrets.my-password.neededForUsers = true;
+
   security.sudo.wheelNeedsPassword = false;
 
-  # FIXME: uncomment the next line to enable SSH
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
   users.mutableUsers = true ;
   users.users.${username} = {
     isNormalUser = true;
-    # FIXME: change your shell here if you don't want fish
     shell = pkgs.fish;
     extraGroups = [
       "wheel"
-      # FIXME: uncomment the next line if you want to run docker without sudo
       "docker"
       "networkmanager"
     ];
-    # FIXME: add your own hashed password
-    # hashedPassword = "";
-    # FIXME: add your own ssh public key
-    # openssh.authorizedKeys.keys = [
-    #   "ssh-rsa ..."
-    # ];
+     hashedPassword = " config.sops.secrets.my-password.path ";
+     openssh.authorizedKeys.keys = [
+       " ~/.config/sops/age/secrets.yaml ..."
+     ];
   };
 
   home-manager.users.${username} = {
@@ -68,23 +66,48 @@
     autoPrune.enable = true;
   };
 
-  # FIXME: uncomment the next block to make vscode running in Windows "just work" with NixOS on WSL
-  # solution adapted from: https://github.com/K900/vscode-remote-workaround
-  # more information: https://github.com/nix-community/NixOS-WSL/issues/238 and https://github.com/nix-community/NixOS-WSL/issues/294
-  # systemd.user = {
-  #   paths.vscode-remote-workaround = {
-  #     wantedBy = ["default.target"];
-  #     pathConfig.PathChanged = "%h/.vscode-server/bin";
-  #   };
-  #   services.vscode-remote-workaround.script = ''
-  #     for i in ~/.vscode-server/bin/*; do
-  #       if [ -e $i/node ]; then
-  #         echo "Fixing vscode-server in $i..."
-  #         ln -sf ${pkgs.nodejs_18}/bin/node $i/node
-  #       fi
-  #     done
-  #   '';
-  # };
+   systemd.user = {
+     paths.vscode-remote-workaround = {
+       wantedBy = ["default.target"];
+       pathConfig.PathChanged = "%h/.vscode-server/bin";
+     };
+     services.vscode-remote-workaround.script = ''
+       for i in ~/.vscode-server/bin/*; do
+         if [ -e $i/node ]; then
+           echo "Fixing vscode-server in $i..."
+           ln -sf ${pkgs.nodejs_18}/bin/node $i/node
+         fi
+       done
+     '';
+   };
+
+# contribution given to takota/keybase.nix https://gist.github.com/taktoa/3133a4d9b1614fad1f4841f145441406
+
+services.kbfs = {
+    enable = true;
+    mountPoint = "%t/kbfs";
+    extraFlags = [ "-label %u" ];
+  };
+
+  systemd.user.services = {
+    keybase.serviceConfig.Slice = "keybase.slice";
+
+    kbfs = {
+      environment = { KEYBASE_RUN_MODE = "prod"; };
+      serviceConfig.Slice = "keybase.slice";
+    };
+
+    keybase-gui = {
+      description = "Keybase GUI";
+      requires = [ "keybase.service" "kbfs.service" ];
+      after    = [ "keybase.service" "kbfs.service" ];
+      serviceConfig = {
+        ExecStart  = "${pkgs.keybase-gui}/share/keybase/Keybase";
+        PrivateTmp = true;
+        Slice      = "keybase.slice";
+      };
+    };
+  };
 
   nix = {
     settings = {
